5e7959713d442e983c35c07a8537c95454a69cfc
Александр Кирин <ggsakir@yandex.ru>
2023-01-12 16:18:26 +0300

    site

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    template = 'posts\index.html'
+    title = 'Это главная страница проекта Yatube'
+    context = {
+        'title': title,
+        'text': 'Последние обновления на сайте',
+    }
+    return render(request, template, context)
+
+def group_posts(request, slug):
+    template = 'posts\group_list.html'
+    title = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'title': title
+    }
+    return render(request, template, context)
